1250fe3e1331008b0dbbf79656eb57566aa6589c
mk1990h2 <mikass111@yahoo.co.jp>
2020-04-21 17:09:26 -0700

    working on assignment3

---
+++ Assignment3/Assignment3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Assignment3
+//
+//  Created by 佐藤美佳 on 2020/04/21.
+//  Copyright © 2020 Mika Sato. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
